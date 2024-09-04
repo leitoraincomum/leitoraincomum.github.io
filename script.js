40e7a084b3c2da766c0a16ce64547fe63f93f584
Fernanda Souza <fernanda.souza-ext@ab-inbev.com>
2024-09-04 18:05:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
