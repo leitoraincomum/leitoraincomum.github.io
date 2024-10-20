214f8516591043f43b954b11ff08428b92c82479
leitoraincomum <fcristinadesouza@gmail.com>
2024-10-20 19:53:06 -0300

    att menu, icons e conteúdo

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// script.js
-
 const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
 
 burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    nav.classList.toggle('nav-active');
+});
+
+// Fechar o menu ao clicar em qualquer link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('nav-active');
+    });
 });
